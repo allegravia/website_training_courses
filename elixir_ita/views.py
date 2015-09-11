c647d3fff44431de6d9993b262adddf6d3bbd90e
allegravia <allegra.via@gmail.com>
2015-09-11 16:56:32 +0200

    Added one course (05.10.2015) and nearly completed RNASeq2015

--- elixir_ita/views.py
+++ elixir_ita/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.shortcuts import redirect
 from .models import Course
 
 #from django.template import RequestContext
@@ -11,9 +12,13 @@ def course_main_page(request):
     return render(request,'elixir_ita/course_main_page.html', {'courses':courses})
 
 def course_detail(request, pk):
+    import os.path
     course = get_object_or_404(Course, pk=pk)
-    return render(request, 'elixir_ita/course_detail_'+pk+'.html', {'course': course})
-
+    if os.path.exists('elixir_ita/templates/elixir_ita/course_detail_'+pk+'.html'):
+	return render(request, 'elixir_ita/course_detail_'+pk+'.html', {'course': course})
+    else:
+	return redirect('https://elearning2.uniroma1.it/course/view.php?id=3221') 
+	
 def application(request, pk):
     course = get_object_or_404(Course, pk=pk)
     return render(request, 'elixir_ita/application.html', {'course': course})
