db0da96fe6ec614fd03150249d65f2ab801ee9fb
allegravia <allegra.via@gmail.com>
2015-10-18 16:41:50 +0200

    added link to material to course pages

--- elixir_ita/urls.py
+++ elixir_ita/urls.py
@@ -2,10 +2,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+#comment next line if you want elixir_ita_homepage to be the homepage
     url(r'^$', views.course_main_page, name='course_main_page'),
+    url(r'^$', views.elixir_ita_homepage, name='elixir_ita_homepage'),
+    url(r'^course$', views.course_main_page, name='course_main_page'),
     url(r'^course/(?P<pk>[0-9]+)/$', views.course_detail, name='course_detail'),
     url(r'^course/(?P<pk>[0-9]+)/application$', views.application, name='application'),
     url(r'^course/(?P<pk>[0-9]+)/programme$', views.programme, name='programme'),
+    url(r'^course/(?P<pk>[0-9]+)/material$', views.material, name='material'),
+    url(r'^course$', views.unix, name='unix'),
 
 ]
 
--- elixir_ita/views.py
+++ elixir_ita/views.py
@@ -11,6 +11,11 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'elixir_ita/templates/elixir_ita/')
 
 # Create your views here.
 
+def elixir_ita_homepage(request):
+    #course = Course()
+    #return render(request, 'elixir_ita/elixir_ita_homepage.html', {'course':course})
+    return render(request, 'elixir_ita/elixir_ita_homepage.html')
+
 def course_main_page(request):
     courses = Course.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request,'elixir_ita/course_main_page.html', {'courses':courses})
@@ -30,3 +35,11 @@ def application(request, pk):
 def programme(request, pk):
     course = get_object_or_404(Course, pk=pk)
     return render(request, 'elixir_ita/programme_'+pk+'.html', {'course': course})
+
+def material(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+    return render(request, 'elixir_ita/material_'+pk+'.html', {'course': course})
+
+def unix(request):
+    course = get_object_or_404(Course, pk=pk)
+    return render(request, 'elixir_ita/unix.html', {'course': course})
